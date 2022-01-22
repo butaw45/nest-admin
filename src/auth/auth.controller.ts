73b885d9898030fb3ee64669bd4dd7506ce54d48
Muhammad Haris <kumantheas@gmail.com>
2022-01-22 15:20:52 +0700

    A: Membuat validasi dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcryptjs';
+import { RegisterDto } from './models/register.dto';
 
 @Controller()
 export class AuthController {
@@ -10,7 +11,11 @@ export class AuthController {
     }
     
     @Post('register')
-    async register(@Body() body) {
+    async register(@Body() body: RegisterDto) {
+        if(body.password !== body.password_confirm){
+            throw new BadRequestException('Password do not match!');
+        }
+
         const hashed = await bcrypt.hash(body.password, 12);
         return this.userService.create({
             first_name: body.first_name,
