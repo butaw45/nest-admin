5fc2549472942dc130b77cda9d55afe2934d1a0c
Muhammad Haris <kumantheas@gmail.com>
2022-01-25 19:25:17 +0700

    A: Abstract service & reuse every service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,56 +1,64 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from 'src/common/abstract.service';
 import { Repository } from 'typeorm';
 import { User } from './models/user.entity';
+import { PaginatedResult } from '../common/paginated-result.interface';
 
 @Injectable()
-export class UserService {
+export class UserService extends AbstractService {
     constructor(
         @InjectRepository(User) private readonly userRepository: Repository<User>
-    ) {}
-
-    async all(): Promise<User[]>{
-        return this.userRepository.find();
+    ) {
+        super(userRepository);
     }
 
-    async paginate(page = 1): Promise<any>{
-        const take = 15;
+    // async all(): Promise<User[]>{
+    //     return this.userRepository.find();
+    // }
 
-        const [users, total] = await this.userRepository.findAndCount({
-            take,
-            skip: (page - 1) * take
-        });
+    async paginate(page = 1, relations = []): Promise<PaginatedResult>{
+        const {data, meta} = await super.paginate(page,relations);
+
+        // const take = 15;
+
+        // const [users, total] = await this.userRepository.findAndCount({
+        //     take,
+        //     skip: (page - 1) * take
+        // });
 
         return{
-            data: users.map(user => {
+            data: data.map(user => {
                 // ? paginate dengan tidak return password
                 const {password, ...data} = user;
 
                 return data;
             }),
-            meta:{
-                total,
-                page,
-                last_page: Math.ceil(total / take)
-            }
+            meta
+            // meta:
+            // {
+                // total,
+                // page,
+                // last_page: Math.ceil(total / take)
+            // }
 
         }
     }
 
-    async create(data): Promise<User>{
-        return this.userRepository.save(data);
+    // async create(data): Promise<User>{
+    //     return this.userRepository.save(data);
 
-    }
+    // }
 
-    async findOne(condition): Promise<User>{
-        return this.userRepository.findOne(condition);
-    } 
+    // async findOne(condition): Promise<User>{
+    //     return this.userRepository.findOne(condition);
+    // } 
 
-    async update(id: number, data): Promise<any>{
-        return this.userRepository.update(id, data);
-        }
+    // async update(id: number, data): Promise<any>{
+    //     return this.userRepository.update(id, data);
+    //     }
 
-    async delete(id: number): Promise<any>{
-        return this.userRepository.delete(id);
-    }
+    // async delete(id: number): Promise<any>{
+    //     return this.userRepository.delete(id);
+    // }
 }
