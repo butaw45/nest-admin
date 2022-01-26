900f6f1d66fda8d41d74b8a471321a47a26e6a8d
Muhammad Haris <kumantheas@gmail.com>
2022-01-26 07:11:34 +0700

    M: Auth change user data, Circular Dependenci

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 // import { JwtModule } from '@nestjs/jwt';
 import { CommonModule } from '../common/common.module';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [
-    UserModule,
+    // UserModule,
+    // Menggunakan forwardRef untuk menghindari circular dependency
+    forwardRef(() => UserModule),
     CommonModule
   ],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService]
 })
 export class AuthModule {}
