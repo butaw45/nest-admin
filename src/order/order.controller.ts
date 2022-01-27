8ceee2f0c4d8bdf907a3ac719950d2395970e883
Muhammad Haris <kumantheas@gmail.com>
2022-01-27 07:17:50 +0700

    A: Export CSV

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Get, Query, UseGuards, UseInterceptors, ClassSerializerInte
 import { OrderService } from './order.service';
 import { AuthGuard } from '../auth/auth.guard';
 import {Response} from 'express'
+import { Parser } from 'json2csv';
+import { Order } from './order.entity';
+import { OrderItem } from './order-item.entity';
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(AuthGuard)
 @Controller()
@@ -18,6 +21,37 @@ export class OrderController {
 
     @Post('export')
     async export(@Res() res: Response) {
-        
+        const parser = new Parser({
+            fields: ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']
+        });
+
+        const orders = await this.orderService.all(['order_items']);
+
+        const json = [];
+        orders.forEach((o: Order) =>{
+            json.push({
+                ID: o.id,
+                Name: o.name,
+                Email: o.email,
+                'Product Title': '',
+                Price: '',
+                Quantity: ''
+            });
+            o.order_items.forEach((i: OrderItem) => {
+                json.push({
+                    ID: '',
+                    Name: '',
+                    Email: '',
+                    'Product Title': i.product_title,
+                    Price: i.price,
+                    Quantity: i.quantity
+                });
+            });
+        });
+
+        const csv = parser.parse(json);
+        res.header('Content-Type', 'text/csv');
+        res.attachment('orders.csv');
+        return res.send(csv);
     }
 }
