810061d8bd08c031cc382dccfa34d7c27d739d54
Muhammad Haris <kumantheas@gmail.com>
2022-01-27 07:02:29 +0700

    A: json2csv @types/json2csv

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Query, UseGuards, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards, UseInterceptors, ClassSerializerInterceptor, Post, Res } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { AuthGuard } from '../auth/auth.guard';
-
+import {Response} from 'express'
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(AuthGuard)
 @Controller()
@@ -14,5 +14,10 @@ export class OrderController {
     async all(@Query('page') page = 1){
         // return this.orderService.all(['order_items']);
         return this.orderService.paginate(page, ['order_items']);
+    }
+
+    @Post('export')
+    async export(@Res() res: Response) {
+        
     }
 }
